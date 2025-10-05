4796d836da287b6abf9b09a25e401642a6180054
xElementzx <xelementzx1@gmail.com>
2025-10-05 16:46:18 +1100

    update svelte to svelte 5

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,8 @@
+import { mount } from "svelte";
 import "./app.pcss";
 import App from "./App.svelte";
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById("app")!,
 });
 
